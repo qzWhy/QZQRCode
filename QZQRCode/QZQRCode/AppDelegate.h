6d2d084ebc53ca80691ad97ab6c2997cc160866b
qzWhy <fanqingzhong1@163.com>
2017-10-17 17:09:05 +0800

    初始化

---
+++ QZQRCode/QZQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QZQRCode
+//
+//  Created by 000 on 17/10/17.
+//  Copyright © 2017年 faner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
